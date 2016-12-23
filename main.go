96d45756e8ccdbc5e276f55665ab67dc6f69c92f
Denis Evsyukov <denis@evsyukov.org>
2016-12-23 16:48:20 +0400

    Move func and consts to different files

--- main.go
+++ main.go
@@ -2,54 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 )
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func HandleYAMLMetaData(datum []byte) (interface{}, error) {
-	m := map[string]interface{}{}
-	err := yaml.Unmarshal(datum, &m)
-	return m, err
-}
 
 func main() {
 	input, err := ioutil.ReadFile("test.md")
-	check(err)
+	Check(err)
 
-	extensions := 0 |
-		blackfriday.EXTENSION_NO_INTRA_EMPHASIS |
-		blackfriday.EXTENSION_TABLES |
-		blackfriday.EXTENSION_FENCED_CODE |
-		blackfriday.EXTENSION_AUTOLINK |
-		blackfriday.EXTENSION_STRIKETHROUGH |
-		blackfriday.EXTENSION_SPACE_HEADERS |
-		blackfriday.EXTENSION_HEADER_IDS |
-		blackfriday.EXTENSION_BACKSLASH_LINE_BREAK |
-		blackfriday.EXTENSION_DEFINITION_LISTS |
-		blackfriday.EXTENSION_FOOTNOTES
-
-	commonHtmlFlags := 0 |
-		blackfriday.HTML_USE_XHTML |
-		blackfriday.HTML_USE_SMARTYPANTS |
-		blackfriday.HTML_SMARTYPANTS_FRACTIONS |
-		blackfriday.HTML_SMARTYPANTS_DASHES |
-		blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+	CheckFrontMatter(input)
 
 	var renderer blackfriday.Renderer
-	renderer = blackfriday.HtmlRenderer(commonHtmlFlags, "", "")
+	renderer = blackfriday.HtmlRenderer(commonHTMLFlags, "", "")
 
 	yaml, err := HandleYAMLMetaData(input)
-	if err != nil {
-		panic(err)
-	}
+	Check(err)
 
 	fmt.Print(yaml)
 	// Need to remove yaml from file
